d3b0f593b97b398150b86635aab739c637404e71
Teodor Potancok <teodor.potancok@gmail.com>
2026-06-26 08:58:05 +0200

    Document fx system

--- src/fx.rs
+++ src/fx.rs
@@ -5,6 +5,19 @@ use bevy::prelude::*;
 use bevy::reflect::{GetTypeRegistration, Typed};
 use std::fmt::Debug;
 
+/**
+Builder for an [AnimationClip] with multiple curves.
+
+Example usage:
+```
+let clip = ClipBuilder::new()
+   .curve(&Name::new("Root"), animate_property!(UiTransform::scale),
+       [0.0, 0.05, 0.1], [1.0, 1.15, 1.1].map(Vec2::splat))
+   .curve(&Name::new("Text"), animate_property!(UiTransform::rotation),
+       [0.0, 0.03, 0.07, 0.1], [0.0, 15.0, -15.0, 0.0].map(Rot2::degrees))
+   .build();
+```
+*/
 pub struct ClipBuilder {
     clip: AnimationClip,
 }
@@ -44,6 +57,15 @@ impl ClipBuilder {
     }
 }
 
+/**
+Creates an [AnimationClip] with one curve.
+
+Example usage:
+```
+let clip = single(&Name::new("Root"), animate_property!(UiTransform::scale),
+    [0.0, 0.05, 0.1], [1.0, 1.15, 1.1].map(Vec2::splat));
+```
+*/
 pub fn single<P>(
     target_name: &Name,
     property: P,
@@ -68,6 +90,32 @@ where
     clip
 }
 
+/**
+A library holding animations by name. You should hold this in a [Resource]
+for reuse.
+
+The builder automatically inserts [AnimationGraph] and [AnimationClip] assets to make
+everything work.
+
+Example code:
+```
+fn setup(
+    mut res: ResMut<MyAnim>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+    mut clips: ResMut<Assets<AnimationClip>>,
+) {
+    let clip: AnimationClip = ...;
+    let other_clip: AnimationClip = ...;
+
+    let library = Library::new()
+        .add("first", clip)
+        .add("second", other_clip)
+        .build(graphs.as_mut(), clips.as_mut());
+
+    res.library = library;
+}
+```
+*/
 #[derive(Default)]
 pub struct Library {
     indexes: HashMap<&'static str, AnimationNodeIndex>,
@@ -84,10 +132,6 @@ impl Library {
 
     pub fn get_index(&self, name: &'static str) -> Option<AnimationNodeIndex> {
         self.indexes.get(name).copied()
-    }
-
-    pub fn get_graph_handle(&self) -> Handle<AnimationGraph> {
-        self.graph_handle.clone()
     }
 }
 
@@ -143,12 +187,49 @@ pub trait GetLibrary {
     fn get_library(&self) -> &Library;
 }
 
+/**
+Marks this entity as animated by `tmpl`.
+
+Example:
+```
+bsn! {
+   #Root
+   Node
+   player::<MyAnim>()
+   target(Name::new("Root"))
+   Children [
+       Text("hi there")
+       animated_by(#Root)
+       target(Name::new("Text"))
+   ]
+}
+```
+*/
 pub fn animated_by(
     tmpl: EntityTemplate,
 ) -> FnTemplate<impl Fn(&mut TemplateContext) -> Result<AnimatedBy> + Clone, AnimatedBy> {
     template(move |ctx| Ok(AnimatedBy(tmpl.build_template(ctx)?)))
 }
 
+/**
+Sets up an [AnimationPlayer] to play animations from a library resource.
+The resource type must implement [GetLibrary].
+
+Example:
+```
+bsn! {
+   #Root
+   Node
+   player::<MyAnim>()
+   target(Name::new("Root"))
+   Children [
+       Text("hi there")
+       animated_by(#Root)
+       target(Name::new("Text"))
+   ]
+}
+```
+*/
 pub fn player<R: Resource + GetLibrary>()
 -> FnTemplate<impl Fn(&mut TemplateContext) -> Result<AnimationPlayer> + Clone, AnimationPlayer> {
     template(move |ctx| {
